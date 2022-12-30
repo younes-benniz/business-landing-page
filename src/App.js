6cf56ad6135db0b7ea861f9b99cddc4c9d7acbc6
younes-benniz <bennizyouness@gmail.com>
2022-12-30 19:18:31 +0100

    add features section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./components/_settings/base.scss";
 import { Header } from "./components/molecules/Header";
 import { HeroSection } from "./components/organisms/HeroSection";
 import { StackSection } from "./components/organisms/StackSection";
+import { FeaturesSection } from "./components/organisms/FeaturesSection";
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<HeroSection />
-			<StackSection />
+			{/* <StackSection /> */}
+			<FeaturesSection />
 		</div>
 	);
 }
---
+++ src/components/organisms/FeaturesSection/FeaturesSection.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FeaturesSection } from ".";
+
+export default {
+	title: "Organisms/Features",
+	component: FeaturesSection,
+};
+
+const Template = (args) => <FeaturesSection {...args} />;
+
+export const Features = Template.bind({});
