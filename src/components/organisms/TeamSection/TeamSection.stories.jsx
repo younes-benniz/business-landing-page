c950194839af3612b4017c50ed92e52d705a441d
younes-benniz <bennizyouness@gmail.com>
2023-01-10 10:30:31 +0100

    add team section

---
+++ src/components/organisms/TeamSection/TeamSection.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TeamSection } from ".";
+
+export default {
+	title: "section/Team",
+	component: TeamSection,
+};
+
+const Template = (args) => <TeamSection {...args} />;
+
+export const Team = Template.bind({});
---
+++ src/components/organisms/TeamSection/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { SvgIcon } from "../../atoms/SvgIcon";
+import "./style.scss";
+
+export const TeamSection = ({ ...props }) => {
+	const arr = [1, 2, 3, 4];
+	return (
+		<section className="team-section">
+			<div className="team-section-title">
+				<h2 className="title">Meet our expert Team</h2>
+			</div>
+			<div className="team-section-description">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, accusamus!</p>
+			</div>
+
+			<div className="team-members">
+				{arr.map((num) => (
+					<div className="member" key={num}>
+						<img
+							className="member-img"
+							src={process.env.PUBLIC_URL + "/team-member.jpg"}
+							alt="team-member"
+						/>
+						<h3 className="member-name">John Doe</h3>
+						<p className="member-role">Front-end dev</p>
+						<div className="social-links">
+							<SvgIcon
+								hasText={false}
+								isLink={true}
+								svgSrc={process.env.PUBLIC_URL + "/email.svg"}
+								size="small"
+							/>
+							<SvgIcon
+								hasText={false}
+								isLink={true}
+								svgSrc={process.env.PUBLIC_URL + "/linkedin.svg"}
+								size="small"
+							/>
+							<SvgIcon
+								hasText={false}
+								isLink={true}
+								svgSrc={process.env.PUBLIC_URL + "/twitter.svg"}
+								size="small"
+							/>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
