a29872aaec698ea41939ee16cad2cc1503bb14c2
younes-benniz <bennizyouness@gmail.com>
2023-01-14 23:05:20 +0100

    add contact section

---
+++ src/components/organisms/ContactSection/ContactSection.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ContactSection } from ".";
+
+export default {
+	title: "section",
+	component: ContactSection,
+};
+
+const Template = (args) => <ContactSection {...args} />;
+
+export const ContactUs = Template.bind({});
---
+++ src/components/organisms/ContactSection/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.scss";
+import { SvgIcon } from "../../atoms/SvgIcon";
+import { Button } from "../../atoms/Button";
+
+export const ContactSection = ({ ...props }) => {
+	return (
+		<section className="contact-section">
+			<h1 className="contact-section-title">CONTACT</h1>
+			<p className="contact-section-description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quaerat facilis
+				suscipit, alias quos dicta pariatur neque maxime earum.
+			</p>
+			<div className="contact-wrapper">
+				<div className="details">
+					<div className="location">
+						<div className="icon">
+							<SvgIcon
+								svgSrc={process.env.PUBLIC_URL + "/map.svg"}
+								size="small"
+								isLink={false}
+								hasText={false}
+							/>
+						</div>
+
+						<div className="detail">
+							<h4>Location:</h4>
+							<p>Lorem ipsum dolor sit.</p>
+						</div>
+					</div>
+				</div>
+				<div className="contact-form">
+					<div className="name-email-wrapper">
+						<div className="name">
+							<label htmlFor="name">Name</label>
+							<input className="contact-input" type="text" id="name" />
+						</div>
+						<div className="email">
+							<label htmlFor="email">Email</label>
+							<input className="contact-input" type="email" id="email" />
+						</div>
+					</div>
+					<div className="subject">
+						<label htmlFor="subject">Subject</label>
+						<input className="contact-input" type="text" id="subject" />
+					</div>
+					<div className="message">
+						<label htmlFor="message">Message</label>
+						<textarea className="contact-input" rows={8} id="message"></textarea>
+					</div>
+					<div className="button">
+						<Button mode={"primary"} size="large" label={"Send"} />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
