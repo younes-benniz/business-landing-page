6d0836214504ed1858aba81104cb085eb6ba6d76
younes-benniz <bennizyouness@gmail.com>
2023-01-26 18:22:40 +0100

    update template to use conditional rendering

--- src/components/templates/HomeTemplate/index.jsx
+++ src/components/templates/HomeTemplate/index.jsx
@@ -17,18 +17,20 @@ export const HomeTemplate = ({
 }) => {
 	return (
 		<>
-			<header className="header">{header}</header>
-			<section className="section hero-section">{hero}</section>
-			<section className="section about-section">{about}</section>
-			<section className="section features-section">{features}</section>
-			<section className="section pricing-section">{pricing}</section>
-			<section className="section technology-section">{technology}</section>
-			<section className="section team-section">{team}</section>
-			<section className="section testimonial-section">{testimonial}</section>
-			<section className="section faq-section">{faq}</section>
-			<section className="section contact-section">{contact}</section>
-			<section className="section subscribe-section">{subscribe}</section>
-			<footer className="footer">{footer}</footer>
+			{header && <header className="header">{header}</header>}
+			{hero && <section className="section hero-section">{hero}</section>}
+			{about && <section className="section about-section">{about}</section>}
+			{features && <section className="section features-section">{features}</section>}
+			{pricing && <section className="section pricing-section">{pricing}</section>}
+			{technology && <section className="section technology-section">{technology}</section>}
+			{team && <section className="section team-section">{team}</section>}
+			{testimonial && (
+				<section className="section testimonial-section">{testimonial}</section>
+			)}
+			{faq && <section className="section faq-section">{faq}</section>}
+			{contact && <section className="section contact-section">{contact}</section>}
+			{subscribe && <section className="section subscribe-section">{subscribe}</section>}
+			{footer && <footer className="footer">{footer}</footer>}
 		</>
 	);
 };
